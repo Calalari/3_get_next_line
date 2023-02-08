65f23c3e4a1887adc02f562cc2ddf103385e17ad
Calalari <68549106+Calalari@users.noreply.github.com>
2023-02-08 16:37:02 +0300

    Update get_next_line_bonus.c

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,29 +6,29 @@
 /*   By: mervyilm <mervyilm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 15:51:44 by mervyilm          #+#    #+#             */
-/*   Updated: 2023/02/04 18:00:00 by mervyilm         ###   ########.fr       */
+/*   Updated: 2023/02/08 16:36:03 by mervyilm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
-void	read_line(char *chr, int fd, char **schr)
+void	read_line(char *chr, int fd, char *schr)
 {
 	char	*temp;
 	int		rd;
 
-	if (!*schr || !ft_strchr(*schr, '\n'))
+	if (!schr || !ft_strchr(schr, '\n'))
 	{
 		rd = read(fd, chr, BUFFER_SIZE);
 		while (rd > 0)
 		{
 			chr[rd] = 0;
-			if (!*schr)
-				*schr = ft_substr(chr, 0, rd);
+			if (!schr)
+				schr = ft_substr(chr, 0, rd);
 			else
 			{
-				temp = *schr;
-				*schr = ft_strjoin(*schr, chr);
+				temp = schr;
+				schr = ft_strjoin(schr, chr);
 				free(temp);
 			}
 			if (ft_strchr(chr, '\n'))
@@ -39,39 +39,39 @@ void	read_line(char *chr, int fd, char **schr)
 	free(chr);
 }
 
-static int	fin(int point, char **schr)
+static int	fin(int point, char *schr)
 {
 	if (point == 1)
-		return (ft_strlen(*schr) - ft_strlen(ft_strchr(*schr, '\n')) + 1);
+		return (ft_strlen(schr) - ft_strlen(ft_strchr(schr, '\n')) + 1);
 	if (point == 2)
-		return (ft_strlen(ft_strchr(*schr, '\n')) - 1);
+		return (ft_strlen(ft_strchr(schr, '\n')) - 1);
 	return (0);
 }
 
-char	*not_new_line(char **schr)
+char	*not_new_line(char *schr)
 {
 	char	*temp;
 	char	*line;
 
-	if (!*schr)
+	if (!schr)
 		return (NULL);
-	if (!ft_strchr(*schr, '\n'))
+	if (!ft_strchr(schr, '\n'))
 	{
-		if (!ft_strlen(*schr))
+		if (!ft_strlen(schr))
 		{
-			free(*schr);
-			*schr = 0;
+			free(schr);
+			schr = 0;
 			return (NULL);
 		}
-		line = ft_substr(*schr, 0, ft_strlen(*schr));
-		free(*schr);
-		*schr = 0;
+		line = ft_substr(schr, 0, ft_strlen(schr));
+		free(schr);
+		schr = 0;
 	}
 	else
 	{
-		line = ft_substr(*schr, 0, fin(1, schr));
-		temp = *schr;
-		*schr = ft_substr(ft_strchr(*schr, '\n'), 1, fin(2, schr));
+		line = ft_substr(schr, 0, fin(1, schr));
+		temp = schr;
+		schr = ft_substr(ft_strchr(schr, '\n'), 1, fin(2, schr));
 		free(temp);
 	}
 	return (line);
@@ -79,20 +79,20 @@ char	*not_new_line(char **schr)
 
 char	*get_next_line_bonus(int fd)
 {
-	static char	*schr[256];
-	char		*chr;
+	static char	schr[256];
+	char		chr[fd];
 
-	chr = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!chr)
+	chr[fd] = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!chr[fd])
 		return (NULL);
-	if (read(fd, chr, 0) == -1 || fd == -1 || BUFFER_SIZE < 1)
+	if (read(fd, chr[fd], 0) == -1 || fd == -1 || BUFFER_SIZE < 1)
 	{
-		free(chr);
+		free(chr[fd]);
 		free(schr);
 		return (NULL);
 	}
-	read_line(chr, fd, &schr);
-	return (not_new_line(&schr));
+	read_line(chr[fd], fd, schr);
+	return (not_new_line(schr));
 }
 
 // #include <fcntl.h>

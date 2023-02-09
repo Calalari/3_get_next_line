60046fceba999d90d913fed8ed854df1f40066b1
Calalari <68549106+Calalari@users.noreply.github.com>
2023-02-09 17:41:58 +0300

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,11 @@
 /*   By: mervyilm <mervyilm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 15:51:44 by mervyilm          #+#    #+#             */
-/*   Updated: 2023/02/02 14:20:46 by mervyilm         ###   ########.fr       */
+/*   Updated: 2023/02/09 16:52:15 by mervyilm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
-#include <fcntl.h>
 
 void	read_line(char *chr, int fd, char **schr)
 {
@@ -66,14 +64,13 @@ char	*not_new_line(char **schr)
 		}
 		line = ft_substr(*schr, 0, ft_strlen(*schr));
 		free(*schr);
+		*schr = 0;
+		return (line);
 	}
-	else
-	{
-		line = ft_substr(*schr, 0, fin(1, schr));
-		temp = *schr;
-		*schr = ft_substr(ft_strchr(*schr, '\n'), 1, fin(2, schr));
-		free(temp);
-	}
+	line = ft_substr(*schr, 0, fin(1, schr));
+	temp = *schr;
+	*schr = ft_substr(ft_strchr(*schr, '\n'), 1, fin(2, schr));
+	free(temp);
 	return (line);
 }
 
@@ -82,52 +79,15 @@ char	*get_next_line(int fd)
 	static char	*schr;
 	char		*chr;
 
-	chr = (char *)malloc(sizeof(char) * 100);
+	chr = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!chr)
 		return (NULL);
-	if (read(fd, chr, 0) == -1 || fd == -1 || BUFFER_SIZE <= 0)
+	if (read(fd, chr, 0) == -1 || fd == -1 || BUFFER_SIZE < 1)
 	{
+		free(schr);
 		free(chr);
 		return (NULL);
 	}
 	read_line(chr, fd, &schr);
 	return (not_new_line(&schr));
-}
-
-#include <fcntl.h>
-#include <stdio.h>
-int main()
-{
-	char *str;
-	int line = 0;
-	int fd;
-	
-	//Finding total lines
-	fd = open("a.txt", O_RDONLY, 0777);
-	while((str = get_next_line(fd)) != NULL)
-		line++;
-	close(fd);
-
-	//Finding first line
-	fd = open("a.txt", O_RDONLY, 0777); 
-	printf("Count of lines inside a.txt = %d\n", line);
-	printf("First line = %s", get_next_line(fd));
-	close(fd);
-
-	//Finding a specific line
-	fd = open("a.txt", O_RDONLY, 0777); 
-	int i = 0;
-	while(i++ < 1) // number line - 2
-		get_next_line(fd); // 0 and 1 passed
-		
-	printf("3rd line = %s", get_next_line(fd)); // 2 which is 3 printed out
-	close(fd);
-
-	//Finding the last line
-	fd = open("a.txt", O_RDONLY, 0777); 
-	int j = 0;
-	while(j++ < line)
-		get_next_line(fd);
-		
-	printf("Last line = %s\n", get_next_line(fd));
 }

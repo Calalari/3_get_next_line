223fadf770f920c4489cbf7b856501a030a47631
Calalari <68549106+Calalari@users.noreply.github.com>
2023-02-09 12:13:34 +0300

    Update get_next_line_bonus.c

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,29 +6,29 @@
 /*   By: mervyilm <mervyilm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 15:51:44 by mervyilm          #+#    #+#             */
-/*   Updated: 2023/02/08 16:36:03 by mervyilm         ###   ########.fr       */
+/*   Updated: 2023/02/09 12:10:49 by mervyilm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
-void	read_line(char *chr, int fd, char *schr)
+void	read_line(char *chr, int fd, char **schr)
 {
 	char	*temp;
 	int		rd;
 
-	if (!schr || !ft_strchr(schr, '\n'))
+	if (!schr[fd] || !ft_strchr(schr[fd], '\n'))
 	{
 		rd = read(fd, chr, BUFFER_SIZE);
 		while (rd > 0)
 		{
 			chr[rd] = 0;
-			if (!schr)
-				schr = ft_substr(chr, 0, rd);
+			if (!schr[fd])
+				schr[fd] = ft_substr(chr, 0, rd);
 			else
 			{
-				temp = schr;
-				schr = ft_strjoin(schr, chr);
+				temp = schr[fd];
+				schr[fd] = ft_strjoin(schr[fd], chr);
 				free(temp);
 			}
 			if (ft_strchr(chr, '\n'))
@@ -48,51 +48,51 @@ static int	fin(int point, char *schr)
 	return (0);
 }
 
-char	*not_new_line(char *schr)
+char	*not_new_line(char **schr, int fd)
 {
 	char	*temp;
 	char	*line;
 
-	if (!schr)
+	if (!schr[fd])
 		return (NULL);
-	if (!ft_strchr(schr, '\n'))
+	if (!ft_strchr(schr[fd], '\n'))
 	{
-		if (!ft_strlen(schr))
+		if (!ft_strlen(schr[fd]))
 		{
-			free(schr);
-			schr = 0;
+			free(schr[fd]);
+			schr[fd] = 0;
 			return (NULL);
 		}
-		line = ft_substr(schr, 0, ft_strlen(schr));
-		free(schr);
-		schr = 0;
+		line = ft_substr(schr[fd], 0, ft_strlen(schr[fd]));
+		free(schr[fd]);
+		schr[fd] = 0;
 	}
 	else
 	{
-		line = ft_substr(schr, 0, fin(1, schr));
-		temp = schr;
-		schr = ft_substr(ft_strchr(schr, '\n'), 1, fin(2, schr));
+		line = ft_substr(schr[fd], 0, fin(1, schr[fd]));
+		temp = schr[fd];
+		schr[fd] = ft_substr(ft_strchr(schr[fd], '\n'), 1, fin(2, schr[fd]));
 		free(temp);
 	}
 	return (line);
 }
 
-char	*get_next_line_bonus(int fd)
+char	*get_next_line(int fd)
 {
-	static char	schr[256];
-	char		chr[fd];
+	static char	*schr[256];
+	char		*chr;
 
-	chr[fd] = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!chr[fd])
+	chr= (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!chr)
 		return (NULL);
-	if (read(fd, chr[fd], 0) == -1 || fd == -1 || BUFFER_SIZE < 1)
+	if (read(fd, chr, 0) == -1 || fd == -1 || BUFFER_SIZE < 1)
 	{
-		free(chr[fd]);
+		free(chr);
 		free(schr);
 		return (NULL);
 	}
-	read_line(chr[fd], fd, schr);
-	return (not_new_line(schr));
+	read_line(chr, fd, schr);
+	return (not_new_line(schr, fd));
 }
 
 // #include <fcntl.h>

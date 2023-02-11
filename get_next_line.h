9b938d4e81c3719986418b3b26eeeb39d3e07b43
Calalari <68549106+Calalari@users.noreply.github.com>
2023-02-11 11:47:12 +0300

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mervyilm <mervyilm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 15:53:09 by mervyilm          #+#    #+#             */
-/*   Updated: 2023/01/30 17:22:34 by mervyilm         ###   ########.fr       */
+/*   Updated: 2023/02/11 11:46:15 by mervyilm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,16 @@
 # define GET_NEXT_LINE_H
 # include <stdlib.h>
 # include <unistd.h>
+# include <limits.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif 
 
 char	*ft_strchr(const char *s, int c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strdup(const char *s1);
 size_t	ft_strlen(const char *s);
+char	*get_next_line(int fd);
 
-#endif
+#endif 

20af6e1baf911db442dfbd99a118dff31bc479a4
Calalari <68549106+Calalari@users.noreply.github.com>
2023-02-13 15:19:15 +0300

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_bonus.h"
+#include "get_next_line.h"
 
 void	read_line(char *chr, int fd, char **schr)
 {
